9bc03740eb694664bc59723c950d6221f54ebc20
Joshua Dean <joshualukedean@gmail.com>
2020-02-12 13:09:28 +0000

    updated ui

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ const router = express.Router();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-//Sets up public folder
+//Sets up public folder for the sites local assetts
 app.use('/public', express.static(__dirname + "/public"));
 
 //Configure Body Parser for Posts
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+//Simple routes to render pages
 app.get('/', function (req, res) {
 	res.render('pages/index', {});
 })
 app.get('/settings', function (req, res) {
 	res.render('pages/settings', {});
 })
-
+//open up the server on the default env port or 8080
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- public/js/script.js
+++ public/js/script.js
@@ -2,25 +2,29 @@
 
 //Assign the graph context to a variable so we can edit it
 var numOfDevCtx = document.getElementById('myChart').getContext('2d');
+var date = new Date();
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 //Set the context to a Chart object
 var numOfDevicesChart = new Chart(numOfDevCtx, {
     // The type of chart we want to create
     type: 'line',
     // The data for our dataset
     data: {
+
         //X Axis tags, Left to Right
-        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        labels: [days[date.getDay() - 7], days[date.getDay() - 6], days[date.getDay() - 5], days[date.getDay() - 4], days[date.getDay() - 3], days[date.getDay() - 2], 'Now'],
         datasets: [{
             //Line Background Transparent
             backgroundColor: 'rgb(255, 99, 132, 0)',
             //Color of the Line
-            borderColor: '#9f00ff',
+            borderColor: '#0d274d',
             //Date from left to right, should be length 7
             data: [0, 10, 5, 2, 20, 30, 45],
-            pointRadius: 5,
+            pointRadius: 0,
             pointHoverRadius: 5,
             pointBackgroundColor: '#9f00ff',
-            lineTension: 0.25
+            lineTension: 0.3,
+            backgroundColor: '#00bceb'
         }]
     },
 
@@ -64,3 +68,9 @@ var deviceTypeChart = new Chart(deviceTypeCtx, {
         }
     }
 });
+function openModal(id) {
+    $('#' + id).before('<div id="' + id + '-placeholder"></div>').detach().appendTo('body').removeClass('hide');
+}
+function closeModal(id) {
+    $('#' + id).detach().prependTo(('#' + id + '-placeholder')).addClass('hide');
+}
